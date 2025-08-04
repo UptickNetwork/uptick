6ea708dc787cf8edfcb6656c31d85e3e2f28b1a7
yangchuantongStarry <chuantong.yang@starrymedia.com>
2025-08-04 18:17:15 +0800

    Modify Vesting Exception

--- app/rpc/override.go
+++ app/rpc/override.go
@@ -4,12 +4,15 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/vesting"
+	vestingtypes "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
 
 	"github.com/UptickNetwork/uptick/app/keepers"
 )
 
 var overrideModules = map[string]overrideHandler{
-	authtypes.ModuleName: overrideAuthServices,
+	authtypes.ModuleName:    overrideAuthServices,
+	vestingtypes.ModuleName: overrideVestingServices,
 }
 
 type overrideHandler func(cdc codec.Codec, configurator module.Configurator, appKeepers keepers.AppKeepers)
@@ -26,4 +29,10 @@ func RegisterService(cdc codec.Codec, mod module.AppModule, configurator module.
 	if ok {
 		m.RegisterServices(configurator)
 	}
+}
+
+func overrideVestingServices(cdc codec.Codec, configurator module.Configurator, appKeepers keepers.AppKeepers) {
+	// vesting module uses grpc.ServiceRegistrar instead of module.Configurator
+	// so we need to register it manually
+	vestingtypes.RegisterMsgServer(configurator.MsgServer(), vesting.NewMsgServerImpl(appKeepers.AccountKeeper, appKeepers.BankKeeper))
 }
